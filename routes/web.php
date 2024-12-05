f654b4ab467cba01d17a2eeee70d781c46dafe69
superern <ernandrewgregorio@gmail.com>
2024-12-04 22:26:44 -0600

    welcome message

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'Laravel' => app()->version(),
+        'message' => 'Welcome to Rachel AI'
+    ];
 });
 
 require __DIR__.'/auth.php';
