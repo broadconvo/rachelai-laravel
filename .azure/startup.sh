96193f8cedfb9693d775c20232aae4c92674bdf0
superern <ernandrewgregorio@gmail.com>
2024-12-18 17:21:23 -0600

    fix startup

--- .azure/startup.sh
+++ .azure/startup.sh
@@ -1,24 +1,20 @@
 # name this file as "startup.sh" and call it from "startup command" as "/home/startup.sh"
-# copy .azure files to /home except set-env-vars.sh
-cp -r .azure/* /home
-rm /home/set-env-vars.sh
-
-bash /home/zsh-setup.sh
-bash /home/apt-setup.sh
-bash /home/composer-setup.sh
+bash /home/site/wwwroot/.azure/zsh-setup.sh
+bash /home/site/wwwroot/.azure/apt-setup.sh
+bash /home/site/wwwroot/.azure/composer-setup.sh
 
 echo "======================================================== START"
 echo " Additional Setups"
 echo "========================================================"
 
 echo "Copying nginx configuration"
-cp /home/default /etc/nginx/sites-enabled/default
+cp /home/site/wwwroot/.azure/nginx-default /etc/nginx/sites-enabled/default
 
 echo "Copying php.ini configuration"
-cp /home/php.ini /usr/local/etc/php/conf.d/php.ini
+cp /home/site/wwwroot/.azure/php.ini /usr/local/etc/php/conf.d/php.ini
 
 echo "Copying laravel worker configuration"
-cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
+cp /home/site/wwwroot/.azure/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
 
 echo "Restarting nginx"
 service nginx restart
@@ -27,4 +23,4 @@ echo "Restarting supervisor"
 service supervisor restart
 echo "======================================================== END"
 
-bash /home/laravel-setup.sh
+bash /home/site/wwwroot/.azure/laravel-setup.sh
