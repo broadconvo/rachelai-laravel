4721cc76d4bb7f05bb6cb55274d9879b5a7687d5
superern <ernandrewgregorio@gmail.com>
2024-12-19 03:55:52 -0600

    additional logs

--- app/Console/Commands/ProcessGmailMessages.php
+++ app/Console/Commands/ProcessGmailMessages.php
@@ -112,6 +112,7 @@ class ProcessGmailMessages extends Command
         } catch (\Exception $e) {
             $this->error("Process failed for User: {$user->email}");
             Log::error("Error processing User: {$user->email}: " . $e->getMessage());
+            Log::error($e->getTraceAsString());
         }
     }
 }
