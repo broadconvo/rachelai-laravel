a24d5b5914d5b4ce6086478d29fcf2aea4ef3e97
superern <ernandrewgregorio@gmail.com>
2024-12-19 15:43:28 -0600

    use :work instead of :run and do it without overlapping

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(ProcessGmailMessages::class)->everyMinute();
+Schedule::command(ProcessGmailMessages::class)->everyMinute()->withoutOverlapping();
