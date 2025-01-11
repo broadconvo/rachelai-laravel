1110cc54a6ef2122afe90add9912ab69af787917
superern <ernandrewgregorio@gmail.com>
2025-01-11 16:34:59 -0600

    add addwin configuration
    includes rachel, broadconvo, crm configs

--- config/addwin.php
+++ config/addwin.php
@@ -2,8 +2,32 @@
 
 use Illuminate\Support\Str;
 
+$defaultUrl = env('RACHEL_URL', 'http://localhost:3000') ;
 return [
-    'extension' => [
-        'password' => env('BROADCONVO_EXTENSION_PASSWORD', 'default-password')
-    ]
+    'broadconvo' => [
+        'extension' => [
+            'password' => env('BROADCONVO_EXTENSION_PASSWORD', 'default-password')
+        ]
+    ],
+    'rachel' => [
+        'url' => [
+            'email' => env('RACHEL_EMAIL_QUERY', $defaultUrl.'/email/query'),
+            'chat' => env('RACHEL_CHAT_QUERY', $defaultUrl.'/chat/query'),
+            'voice' => [
+                'query' => env('RACHEL_VOICE_QUERY', $defaultUrl.'/voice/query'),
+                'filler' => env('RACHEL_VOICE_FILLER', $defaultUrl.'/voice/filler'),
+                'info' => env('RACHEL_VOICE_INFO', $defaultUrl.'/voice/info'),
+            ],
+            'knowledgeBase' => [
+                // POST: Uploads document like pdf, txt, csv, pptx, docx
+                'upload' => env('RACHEL_KB_UPLOAD', $defaultUrl.'/kb/upload'),
+                // POST: Put all the contents of the textarea to the knowledgebase
+                'text' => env('RACHEL_KB_TEXT', $defaultUrl.'/kb/upload/text'),
+                // GET: Load the list of existing knowledge base documents
+                // POST: Add new document to the list
+                'list' => env('RACHEL_KB_LIST', $defaultUrl.'/kb/list'),
+            ]
+        ],
+    ],
+    'crm' => []
 ];
