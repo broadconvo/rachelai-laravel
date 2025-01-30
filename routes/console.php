29b7a729f1c26e3776b35b39bea319d2a5191d3c
superern <ernandrewgregorio@gmail.com>
2025-01-29 18:09:03 -0600

    remove inspiring quotes in console

--- routes/console.php
+++ routes/console.php
@@ -6,10 +6,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
 Schedule::command(GenerateDraftCommand::class)
     ->everyTwoMinutes()
     ->withoutOverlapping(300); // Lock expires after 5 minutes
