973d1ed3b04b435d651487ac6ea6095cf542a897
superern <ernandrewgregorio@gmail.com>
2025-02-05 13:22:49 -0600

    create new rule for checking PhoneExtensionNotExists when creating tenant

---
+++ app/Rules/PhoneExtensionNotExists.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Broadconvo\PhoneExtension;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class PhoneExtensionNotExists implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $exists = PhoneExtension::where('extension_number', $value)->exists();
+
+        if ($exists) {
+            $fail("The phone extension already exists.");
+        }
+    }
+}
